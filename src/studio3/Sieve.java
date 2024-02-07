d5a7796abd39d5df37cfc6f2c5cb30b2cee01332
izzie <izzie@izziescomputer.dhcp.wustl.edu>
2024-02-07 15:48:19 -0600

    a

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,28 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("please enter a value for n: ");
+		int n = in.nextInt();
+		int[] sieve=new int[n];
+		boolean[] isPrime=new boolean[n];
+		int[] primeNumbers= {2,3,5,7,11,13,17,19,23,29,31, 37};
+		for (int i = 0; i<n; i++) {
+			sieve[i]=i+2;
+		}
+		for (int i = 0; i<n; i++) {
+			sieve[i]=
+					
+			
+			
+		}
+		
+		
+
+	}
+
+}
